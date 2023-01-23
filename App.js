786b6ae101d66f3a63249f98913f48b8aa3eed3b
yoona1110 <gjdbsdk18@icloud.com>
2023-01-24 03:39:19 +0900

    로그인 페이지 ui 완성

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>와 신기하다!</Text>
+      <Text style={styles.title}> 니캉내캉 </Text>
+
+      <View style={styles.idInputTheme}>
+        <Text style={styles.inputTitle}> 아이디 </Text>
+        <TextInput style={styles.idInput}/> 
+      </View>
+      
+      <View style={styles.pwInputTheme}>
+        <Text style={styles.inputTitle}> 비밀번호 </Text>
+        <TextInput style={styles.pwInput} secureTextEntry='true'/> 
+      </View>
+
+      {/* 특정 영역을 지정*/}
+      <TouchableOpacity style={styles.btn_SignUp}>
+        <Text style={styles.btnFont}> 회원가입 </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btn_Login}>
+        <Text style={styles.btnFont}> 로그인 </Text>
+      </TouchableOpacity>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,76 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFEDAB',
+    alignItems: 'center'
   },
+
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: '#575757',
+    marginTop: 220
+  },
+
+  inputTitle: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 12
+  },
+
+  idInputTheme: {
+    flexDirection: 'row',
+    marginTop: 58,
+    marginLeft: 9
+  },
+
+  pwInputTheme: {
+    flexDirection: 'row',
+    marginTop: 15
+  },
+
+  idInput: {
+    width: 220,
+    height: 50,
+    fontSize: 18,
+    padding: 10,
+    borderRadius: 10,
+    marginLeft: 25,
+    backgroundColor: '#FAFAFA'
+  },
+
+  pwInput: {
+    width: 220,
+    height: 50,
+    fontSize: 18,
+    padding: 10,
+    borderRadius: 10,
+    marginLeft: 15,
+    backgroundColor: '#FAFAFA'
+  },
+
+  btn_SignUp: {
+    width: 320,
+    height: 60,
+    backgroundColor: '#FFE589',
+    paddingTop: 15,
+    marginTop: 50,
+    borderRadius: 10
+  },
+
+  btnFont: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#575757'
+  },
+
+  btn_Login: {
+    width: 320,
+    height: 60,
+    backgroundColor: '#FFE589',
+    paddingTop: 15,
+    marginTop: 15,
+    borderRadius: 10
+  }
 });
